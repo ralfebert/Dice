5501fda2371610938ae96e506e7951e13d3fb4fa
Ralf Ebert <info@ralfebert.de>
2019-08-31 17:18:11 +0200

    Project setup Dice

---
+++ Dice/App/DiceAppDelegate.swift
@@ -0,0 +1,14 @@
+// © 2019 Ralf Ebert — Dice
+
+import UIKit
+
+@UIApplicationMain
+class DiceAppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
