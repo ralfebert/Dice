e585bef46da2e0d6594fbd7a2670a5e6db962a5e
Ralf Ebert <info@ralfebert.de>
2019-09-02 20:10:07 +0200

    Dice example ported to SwiftUI

--- Dice/App/DiceAppDelegate.swift
+++ Dice/App/DiceAppDelegate.swift
@@ -1,6 +1,7 @@
 // © 2019 Ralf Ebert — iOS Example Project: Dice
 // License: https://opensource.org/licenses/MIT
 
+import SwiftUI
 import UIKit
 
 @UIApplicationMain
@@ -9,6 +10,12 @@ class DiceAppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        let window = UIWindow()
+        window.rootViewController = UIHostingController(rootView: DiceView())
+        window.makeKeyAndVisible()
+        self.window = window
+
         return true
     }
 
