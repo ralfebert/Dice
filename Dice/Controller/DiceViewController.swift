58de7ec0e072f64dad41eebb436dc9102b6eca41
Ralf Ebert <info@ralfebert.de>
2020-02-11 11:27:39 +0100

    implemented dice

--- Dice/Controller/DiceViewController.swift
+++ Dice/Controller/DiceViewController.swift
@@ -5,8 +5,16 @@ import UIKit
 
 class DiceViewController: UIViewController {
 
+    @IBOutlet var diceImageView: UIImageView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func rollDice(_: Any) {
+        let number = (1 ... 6).randomElement()!
+        self.diceImageView.image = UIImage(named: "dice-\(number)")
     }
 
 }
