d4585bc216b3d0bf86a8b79613e0f0d40de25baa
Ralf Ebert <info@ralfebert.de>
2021-10-30 21:38:11 +0200

    simple implementation dice number

--- Dice/ContentView.swift
+++ Dice/ContentView.swift
@@ -1,9 +1,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    let diceNumber = Int.random(in: 1 ... 6)
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing: 15) {
+            Image("dice-\(diceNumber)")
+            Text("You rolled a \(diceNumber)")
+                .bold()
+        }
     }
 }
 
