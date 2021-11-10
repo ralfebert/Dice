5288b1fd664def41a4a299dacc4a3a197e598bb8
Ralf Ebert <info@ralfebert.de>
2021-11-10 15:44:39 +0100

    rename to DiceView

--- Dice/DiceApp.swift
+++ Dice/DiceApp.swift
@@ -4,7 +4,7 @@ import SwiftUI
 struct DiceApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            DiceView()
         }
     }
 }
--- Dice/DiceView.swift
+++ Dice/DiceView.swift
@@ -1,6 +1,6 @@
 import SwiftUI
 
-struct ContentView: View {
+struct DiceView: View {
     let diceNumber = Int.random(in: 1 ... 6)
 
     var body: some View {
@@ -14,6 +14,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        DiceView()
     }
 }
