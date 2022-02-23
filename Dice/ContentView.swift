25cb2090b9516d2762dbc3e5850e2c798f7a7af0
Ralf Ebert <info@ralfebert.de>
2022-02-23 20:01:00 +0100

    non-optional implementation

--- Dice/ContentView.swift
+++ Dice/ContentView.swift
@@ -1,19 +1,17 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var diceNumber: Int?
+    @State var diceNumber: Int = Int.random(in: 1 ... 6)
 
     var body: some View {
         VStack(spacing: 15) {
-            if let diceNumber = self.diceNumber {
-                Image(systemName: "die.face.\(diceNumber).fill")
-                    .imageScale(.large)
-                    .symbolRenderingMode(.hierarchical)
-                    .foregroundColor(.green)
-                    .font(.system(size: 100))
-                Text("You rolled a \(diceNumber)")
-                    .bold()
-            }
+            Image(systemName: "die.face.\(diceNumber).fill")
+                .imageScale(.large)
+                .symbolRenderingMode(.hierarchical)
+                .foregroundColor(.green)
+                .font(.system(size: 100))
+            Text("You rolled a \(diceNumber)")
+                .bold()
             Button("Roll the Dice!") {
                 self.diceNumber = Int.random(in: 1 ... 6)
             }
