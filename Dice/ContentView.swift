fb65535bb79b7a10a7f96e9d52d7771a7acb774f
Ralf Ebert <info@ralfebert.de>
2022-03-01 11:34:42 +0100

    Variante Anzeige mehrerer Würfel

--- Dice/ContentView.swift
+++ Dice/ContentView.swift
@@ -1,21 +1,38 @@
 import SwiftUI
 
+struct Dice: Identifiable {
+    let id = UUID()
+    let number = Int.random(in: 1 ... 6)
+}
+
+struct DiceRoll {
+    let dice: [Dice] = [Dice(), Dice()]
+
+    var totalNumber: Int {
+        dice.map { $0.number }.reduce(0, +)
+    }
+}
+
 struct ContentView: View {
-    @State var diceNumber: Int?
+    @State var diceRoll: DiceRoll?
 
     var body: some View {
         VStack(spacing: 15) {
-            if let diceNumber = self.diceNumber {
-                Image(systemName: "die.face.\(diceNumber).fill")
-                    .imageScale(.large)
-                    .symbolRenderingMode(.hierarchical)
-                    .foregroundColor(.green)
-                    .font(.system(size: 100))
-                Text("You rolled a \(diceNumber)")
+            if let diceRoll = self.diceRoll {
+                HStack {
+                    ForEach(diceRoll.dice) { dice in
+                        Image(systemName: "die.face.\(dice.number).fill")
+                            .imageScale(.large)
+                            .symbolRenderingMode(.hierarchical)
+                            .foregroundColor(.green)
+                            .font(.system(size: 100))
+                    }
+                }
+                Text("\(diceRoll.dice.map { String($0.number) }.formatted()) → \(diceRoll.totalNumber)")
                     .bold()
             }
             Button("Roll the Dice!") {
-                self.diceNumber = Int.random(in: 1 ... 6)
+                self.diceRoll = DiceRoll()
             }
         }
     }
